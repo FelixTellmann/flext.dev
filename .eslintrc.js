ad4eaf3b08918e00218213e4f50ec7cfb9f20dbf
FelixTellmann <felix@tellmann.co.za>
2021-05-22 22:48:41 +0200

    Initial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ["./node_modules/fx-style/"],
+  plugins: [],
+  env: {
+    browser: true,
+  },
+  ignorePatterns: ["public/**/*", "models", "amplify/**/*", "*.md", "*.mdx"],
+  rules: {},
+};
