50a792354fa00b3ff185d9b468f00b77a1e83e02
Felix <felix@tellmann.co.za>
2021-11-06 13:50:21 +0200

    Update

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -8,7 +8,18 @@ type TestData = {
 type TestFunction = (req: NextApiRequest, res: NextApiResponse<TestData>) => Promise<void>;
 
 export const Test: TestFunction = async (req, res) => {
-  console.log("hi");
+  const startTime = Date.now();
+  try {
+    const test = await prisma.test.create({
+      data: {
+        name: "felxi2",
+      },
+    });
+
+    console.log(test, `${Date.now() - startTime}ms`);
+  } catch (err) {
+    console.log(err.message);
+  }
   res.status(200).json({ name: "John Doe" });
 };
 
