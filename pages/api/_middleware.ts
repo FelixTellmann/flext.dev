40ca8eefdc7b42e4d389745f7f3c8f2eff7fd287
Felix <felix@tellmann.co.za>
2021-11-06 14:22:58 +0200

    Updated middleware

--- pages/api/_middleware.ts
+++ pages/api/_middleware.ts
@@ -5,5 +5,6 @@ export const middleware = async (req: NextRequest, ev: NextFetchEvent) => {
   const startTime = Date.now();
   const a = await redis.get("test");
   console.log(a, `${Date.now() - startTime}ms`);
+
   new Response("Hello, world!");
 };
