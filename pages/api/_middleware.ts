115981066d9ed8742acbccb01e5bb1d338055f59
Felix <felix@tellmann.co.za>
2021-11-08 08:14:49 +0200

    test of middleware

--- pages/api/_middleware.ts
+++ pages/api/_middleware.ts
@@ -4,7 +4,7 @@ import { NextFetchEvent, NextRequest } from "next/server";
 export const middleware = async (req: NextRequest, ev: NextFetchEvent) => {
   const startTime = Date.now();
   const a = await redis.get("test");
-  console.log(a, `${Date.now() - startTime}ms`);
+  console.log({ a }, `${Date.now() - startTime}ms`);
 
   new Response("Hello, world!");
 };
