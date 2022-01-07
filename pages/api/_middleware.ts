7459b3ac28489dac41dd9b4a1e144da84135c107
Felix <felix@tellmann.co.za>
2022-01-07 16:02:46 +0200

    Updated tsc errors

--- pages/api/_middleware.ts
+++ pages/api/_middleware.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @next/next/no-server-import-in-page
 import { NextFetchEvent, NextRequest } from "next/server";
 
 const middleware = async (req: NextRequest, ev: NextFetchEvent) => {
