662d9efd673a5205ee132c52054d803f3e1583df
Felix <felix@tellmann.co.za>
2022-04-27 10:44:19 +0200

    Type fixes

--- pages/code-component.tsx
+++ pages/code-component.tsx
@@ -18,21 +18,25 @@ export const getStaticProps: GetStaticProps = async (context) => {
           caption: "Unstyled",
           description: "No need to override styles, no specificity wars.",
           code: fs.readFileSync("./content/examples/test-1.txt", { encoding: "utf-8" }),
+          language: "tsx",
         },
         {
           caption: "Composable",
           description: "Granular access to each component part.",
           code: fs.readFileSync("./content/examples/test-2.txt", { encoding: "utf-8" }),
+          language: "tsx",
         },
         {
           caption: "Customizable",
           description: "Configure behavior, control focus, add event listeners.",
           code: fs.readFileSync("./content/examples/test-3.txt", { encoding: "utf-8" }),
+          language: "tsx",
         },
         {
           caption: "Consistent",
           description: "Components with similar functionality share similar API.",
           code: fs.readFileSync("./content/examples/test-4.txt", { encoding: "utf-8" }),
+          language: "tsx",
         },
       ],
     }, // will be passed to the page component as props
@@ -41,7 +45,12 @@ export const getStaticProps: GetStaticProps = async (context) => {
 
 export default (
   props: JSX.IntrinsicAttributes & {
-    codeBlocks: { caption: string; code: string; description: string }[];
+    codeBlocks: {
+      caption: string;
+      code: string;
+      description: string;
+      language: "javascript" | "typescript" | "jsx" | "tsx";
+    }[];
     filename: string;
     contentBlock?: ContentBlockProps | undefined;
   } & { children?: ReactNode }
